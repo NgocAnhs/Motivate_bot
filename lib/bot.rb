fbc9df2a9258ea7ba708d741caccf17e983631a5
AnhTN <ngocanhgidi@gmail.com>
2021-09-13 13:19:28 +0700

    remove bnb

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,14 +1,8 @@
 require 'telegram/bot'
 require_relative 'motivate.rb'
-require_relative 'binance_api'
 
 class Bot
   def initialize
-    token = '1974224873:AAGiXTtXyiE9Fry2xtn4OivcKPkkbI6snGo'
-    key = "uSVlopSP2eNIxWyOLmXUNiqttfXtRNbJoQinI1DQxLISlFFEEei0WS0fL9zcDzez"
-    secret_key = "icm8zHwQOk5kg6qoBkKsnQB5cXsDmp6887MsD8YBCrmtHA6t19LdMt6FoFvgeqkw"
-    bnb_api = BinanceApi.new(key, secret_key)
-
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
         case message.text
@@ -20,23 +14,10 @@ class Bot
           values = Motivate.new
           value = values.select_random
           bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
-        when '/balance_info'
-          values = bnb_api.account_info
-          mess = get_balances(values)
-          bot.api.send_message(chat_id: message.chat.id, text: mess, date: message.date)
         else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
         end
       end
     end
-  end
-
-  def get_balances(value)
-    messages = []
-    value["balances"].each do |item|
-      next if item["free"].to_i == 0 && item["locked"].to_i == 0
-      messages << "asset: #{item['asset']} \nfree: #{item['free']} \nlocked: #{item['locked']}"
-    end
-    messages.join("\n-----------------\n")
   end
 end
 
